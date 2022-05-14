c456a91cec9e6c388825c3621604309d48ce58b2
luccasfalmeida@outlook.com <luccasfalmeida@outlook.com>
2022-05-14 02:46:51 -0300

    first commit intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,14 @@
+package br.com.dio;
+
+import br.com.dio.model.Gatun;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gatun gato = new Gatun();
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 6;
+        System.out.println("Hello World dnvo! " + (a+b));*/
+    }
+}
